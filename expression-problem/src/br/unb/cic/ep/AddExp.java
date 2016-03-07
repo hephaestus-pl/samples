93c66781e5161e0952cf81452ffec7d8c3f99d98
rbonifacio <rbonifacio123@gmail.com>
2016-03-07 12:45:17 -0500

    variability support to the Exp interface

--- expression-problem/src/br/unb/cic/ep/AddExp.java
+++ expression-problem/src/br/unb/cic/ep/AddExp.java
@@ -2,19 +2,19 @@ package br.unb.cic.ep;
 
 public class AddExp implements Exp {
 
-	private Exp lhs;
-	private Exp rhs; 
+    private Exp lhs;
+    private Exp rhs; 
 	
-	@Override
-	public int eval() {
-		return lhs.eval() + rhs.eval();
-	}
+    @Override
+    public int eval() {
+	return lhs.eval() + rhs.eval();
+    }
 	
-/*#PP*/
-	@Override
-	public String prettyPrint() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-/*#*/	
+    //#ifdef PP
+    @Override
+    public String prettyPrint() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+    //#endif
 }
